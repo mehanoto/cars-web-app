6fbbdb964b48722ad24989f16d264a8705f9f8f7
mehanoto <stefan.stoyanov@pgmtplovdiv.info>
2025-04-02 09:03:17 +0300

    Some php basic examples

---
+++ php-examples/last/test.php
@@ -0,0 +1,11 @@
+<?php
+
+$authors = array("ab", "ac", "ad", "ba", "bc", "bd", "ca", "cd", "ce");
+$prevLetter = substring($authors[0], 0, 1);
+
+for($i = 0; ??? ; ???)
+{
+    //TODO:
+
+    echo "<div> $authors[0] </div>"
+}
